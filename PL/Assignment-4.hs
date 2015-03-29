bc5a2146da9106f8ad7e738976946411a9789a48
Noel Bao <noeleon930@gmail.com>
2015-03-29 19:35:14 +0800

    PL HW4 part 3 bug fix

--- PL/Assignment-4.hs
+++ PL/Assignment-4.hs
@@ -34,7 +34,7 @@ transform st [] = replicate (length st) Delete
 transform [] st = map Insert st
 transform (a:x) (b:y)
     | a == b = Copy : transform x y
-    | otherwise = best [Delete : transform x ([b] ++ y), Insert b : transform ([b] ++ x) y, Change b : transform x y]
+    | otherwise = best [Delete : transform x ([b] ++ y), Insert b : transform ([a] ++ x) y, Change b : transform x y]
 
 cost :: [Edit] -> Int
 cost = length . filter (/=Copy)
